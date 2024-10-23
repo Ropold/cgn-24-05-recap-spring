a3ebe9fc33572f35c87e1a720c05a588c62777bc
Robert Stolz <robert.stolz87@gmail.com>
2024-10-23 14:31:33 +0200

    recap start

---
+++ src/main/java/ropold/cgn2405recapspring/Cgn2405RecapSpringApplication.java
@@ -0,0 +1,13 @@
+package ropold.cgn2405recapspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn2405RecapSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cgn2405RecapSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ropold/cgn2405recapspring/Cgn2405RecapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ropold.cgn2405recapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn2405RecapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
