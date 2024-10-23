8cf56b33701a5dd69b28682be7b4276613b0f537
Robert Stolz <robert.stolz87@gmail.com>
2024-10-23 14:42:23 +0200

    1st setup

--- src/test/java/ropold/cgn2405recapspring/Cgn2405RecapSpringApplicationTests.java
+++ src/test/java/ropold/cgn2405recapspring/Cgn2405RecapSpringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Cgn2405RecapSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
